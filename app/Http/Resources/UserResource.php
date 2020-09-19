5eedd474021c1f877e91b593bfed3a491ed76fa0
Milad <miladfathi021@gmail.com>
2020-09-19 11:02:12 +0430

    add username to User Resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             'name' => $this->name,
             'email' => $this->email,
-            'username' => $this->username,
+            'username' => $this->profile->username,
             'api_token' => $this->api_token->token ?? null,
         ];
     }
