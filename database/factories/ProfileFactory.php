a88a1ea9d6a3468a1673d6c8ed3b523a38929fdb
Milad <miladfathi021@gmail.com>
2020-09-25 20:17:31 +0330

    Create profile factory

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profile\Profile;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProfileFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Profile::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => UserFactory::new()->create()->id,
+            'username' => $this->faker->userName,
+        ];
+    }
+}
