fc79922a948759f0ff1123b4c4112341025d8024
Milad <miladfathi021@gmail.com>
2020-09-26 11:21:01 +0330

    create tweet relationship method

--- app/Models/Profile/Profile.php
+++ app/Models/Profile/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Profile;
 
+use App\Models\Tweet\Tweet;
 use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -14,5 +15,10 @@ class Profile extends Model
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class, 'profile_id');
     }
 }
