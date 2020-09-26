af0c576f28dfc3d4a659f2cc9dfc305d78f6019a
Milad <miladfathi021@gmail.com>
2020-09-26 11:17:26 +0330

    Create tweets controller with routes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/v1'], function () {
-    Route::get('/test', function () {
-        return 'done';
-    })->middleware(['auth:api']);
    require('Auth/user.php');
+   require('Tweet/tweet.php');
 });
