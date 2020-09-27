35b72d8bc36a8e5947cf33f54423df25bc1ce98f
Milad <miladfathi021@gmail.com>
2020-09-27 16:13:17 +0330

    Create tweet controller

--- app/Http/Controllers/V1/Tweet/TweetsController.php
+++ app/Http/Controllers/V1/Tweet/TweetsController.php
@@ -4,10 +4,40 @@ namespace App\Http\Controllers\V1\Tweet;
 
 use App\Http\Controllers\ApiController;
 use App\Http\Requests\Tweet\TweetRequest;
+use App\Http\Requests\Tweet\TweetUpdateRequest;
+use App\Http\Resources\Tweet\TweetCollection;
 use App\Http\Resources\Tweet\TweetResource;
+use App\Models\Tweet\Tweet;
 
 class TweetsController extends ApiController
 {
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $tweet = auth()->user()->profile->tweets()->paginate(25);
+
+        return $this->responseOk(
+            new TweetCollection($tweet)
+        );
+    }
+
+    /**
+     * @param Tweet $tweet
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Tweet $tweet)
+    {
+        return $this->responseOk(
+            new TweetResource($tweet)
+        );
+    }
+
+    /**
+     * @param TweetRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(TweetRequest $request)
     {
         $tweet = auth()->user()->profile->tweets()->create($request->all());
@@ -15,5 +45,30 @@ class TweetsController extends ApiController
         return $this->responseOk(
             new TweetResource($tweet)
         );
+    }
+
+    /**
+     * @param Tweet $tweet
+     * @param TweetUpdateRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Tweet $tweet, TweetUpdateRequest $request)
+    {
+        $tweet->update($request->all());
+
+        return $this->responseOk(
+            new TweetResource($tweet->fresh())
+        );
+    }
+
+    /**
+     * @param Tweet $tweet
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Tweet $tweet)
+    {
+        $tweet->delete();
+
+        return $this->responseOk();
     }
 }
